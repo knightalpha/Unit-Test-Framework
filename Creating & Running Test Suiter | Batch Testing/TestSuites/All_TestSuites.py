09b0856739230c4f8ea214f60e7e1746d945b939
Prashant Singh <pk.airsqn@gmail.com>
2020-05-23 23:38:01 +0530

    Update All_TestSuites.py

--- Creating & Running Test Suiter | Batch Testing/TestSuites/All_TestSuites.py
+++ Creating & Running Test Suiter | Batch Testing/TestSuites/All_TestSuites.py
@@ -5,6 +5,7 @@ from Package1.TC_SignupTest import SignupTest
 from Package2.TC_PaymentTest import PaymentTest
 from Package2.TC_PaymentReturnsTest import PaymentReturnsTest
 
+
 # Get all the tests from LoginTest, SignUpTest, PaymentTest and PaymentReturnsTest
 tc1 = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
 tc2 = unittest.TestLoader().loadTestsFromTestCase(SignupTest)
@@ -13,11 +14,11 @@ tc4 = unittest.TestLoader().loadTestsFromTestCase(PaymentReturnsTest)
 
 # Creating Test Suites
 '''Login and SignUp Tests will come into Sanity Test Suite'''
-sanityTestSuite = unittest.TestSuite([tc1, tc2])    # Sanity Test Suite
+sanityTestSuite = unittest.TestSuite([tc1, tc2])    # Sanity Test Suite 
 # unittest.TextTestRunner().run(sanityTestSuite)
 
 '''Payment and PaymentReturns Test will come into Functional Test Suite'''
-functionalTestSuite = unittest.TestSuite([tc3, tc4])    # Functional Test Suite
+functionalTestSuite = unittest.TestSuite([tc3, tc4])    # Functional Test Suite 
 # unittest.TextTestRunner().run(functionalTestSuite)
 
 '''Rest all the test cases will fall under Master Test Suite'''
